f9916bcdb52f9b20383b218728825910c0615a52
Mario Cortes lozano <mcortes-@c2r6s6.42madrid.com>
2021-02-25 20:34:29 +0100

    25/02/2021 Last changes

---
+++ proy-C-00/ex05/ft_print_comb.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcortes- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/25 19:52:58 by mcortes-          #+#    #+#             */
+/*   Updated: 2021/02/25 20:24:46 by mcortes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_print_comb(void)
+{
+	char primero;
+	char segundo;
+	char tercero;	
+
+	primero = '0';
+	segundo = '1';
+	tercero = '2';
+
+}
+
+int main(void){
+	ft_print_comb();
+}
+
