1178bf729aed849c7548586953e829b4d793e0cf
Mario Cortes lozano <mcortes-@c2r7s6.42madrid.com>
2021-02-28 16:37:03 +0100

    28/02/201 Added .pdfs to projects

--- proy-C-00/ex05/ft_print_comb.c
+++ proy-C-00/ex05/ft_print_comb.c
@@ -6,22 +6,23 @@
 /*   By: mcortes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 19:52:58 by mcortes-          #+#    #+#             */
-/*   Updated: 2021/02/25 20:24:46 by mcortes-         ###   ########.fr       */
+/*   Updated: 2021/02/28 16:29:04 by mcortes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 
+void	ft_putchar(char valor){
+	write(1, &valor, 1);
+}
+
 void	ft_print_comb(void)
 {
-	char primero;
-	char segundo;
-	char tercero;	
+	char a;
+	char b ;
+	char c;
 
-	primero = '0';
-	segundo = '1';
-	tercero = '2';
 
 }
 
