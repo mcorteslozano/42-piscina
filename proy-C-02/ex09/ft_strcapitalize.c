48cf7650233277ccfecd79cd6ab49c611e619bdf
Mario Cortes lozano <mcortes-@c2r8s3.42madrid.com>
2021-03-08 20:53:50 +0100

    08/03/2021

--- proy-C-02/ex09/ft_strcapitalize.c
+++ proy-C-02/ex09/ft_strcapitalize.c
@@ -1,17 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcortes- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/08 19:31:50 by mcortes-          #+#    #+#             */
+/*   Updated: 2021/03/08 20:21:36 by mcortes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
 char	*ft_strcapitalize(char *str)
 {
 	int i;
-
+	
+	i = 0;
+	if(str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] = str[i] - 32;
+	}
+	while(str[i] != '\0')
+	{
+		if(str[i] >= 'a' && str[i] <= 'z' && str[i-1]  >= 32 && str[i-1] <= 47)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	
 	i = 0;
 	while(str[i] != '\0')
 	{
+		printf("%c", str[i]);
+		i++;
+	}
+	return (str);
+}
 
-
-
-
-
-
-
-	hola mi nombre es mario
+int main()
+{
+	char prueba[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+	ft_strcapitalize(&prueba[0]);
+}
--- proy-C-02/ex10/ft_strlcpy.c
+++ proy-C-02/ex10/ft_strlcpy.c
@@ -1,40 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcortes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/08 19:31:09 by mcortes-          #+#    #+#             */
-/*   Updated: 2021/03/08 19:31:13 by mcortes-         ###   ########.fr       */
+/*   Created: 2021/03/08 20:22:27 by mcortes-          #+#    #+#             */
+/*   Updated: 2021/03/08 20:27:56 by mcortes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_str_is_lowercase(char *str)
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	int i;
-	int j;
 
-	i = 0;
-	j = 1;
-	while(str[i] != '\0')
-	{
-		if(str[i] >= 'a' && str[i] <= 'z')
-			j = 1;
-		else
-			return (0);
-		i++;
-	}
-	return (j);
 }
 
-int main(void)
-{
-	int prueba;
-
-	prueba = ft_str_is_lowercase("sada___!!");
-
-	printf("Prueba -> %d\n", prueba);
-}
