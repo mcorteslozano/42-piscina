f584270990a72ec27e8ada879be1679a6f700d2e
Mario Cortes lozano <mcortes-@c2r7s6.42madrid.com>
2021-03-10 18:45:06 +0100

    10/03/2021

--- proy-C-03/ex05/ft_strlcat.c
+++ proy-C-03/ex05/ft_strlcat.c
@@ -1,19 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcortes- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/10 18:09:09 by mcortes-          #+#    #+#             */
+/*   Updated: 2021/03/10 18:09:12 by mcortes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
-
-unsigned int getTam(char *str)
-{
-    int i;
-
-    i = 0;
-    while(str[i] != '\0')
-    {
-        i++;
-    }
-    return (i - 1);
-}
+#include <string.h>
 
 unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
 {
+    unsigned int i;
+    unsigned int j;
+    unsigned int res;
+
+    i = 0;
+    while(dest[i] != '\0')
+    {
+        i++;
+    }
+    res = 0;
+    while(src[res] != '\0')
+    {
+        ++res;
+    }
+    if(size <= i)
+    {
+        res += size;
+    } 
+    else
+    {
+        
+    }
+
 
 }
 
